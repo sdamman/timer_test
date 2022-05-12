c969db2d2d51b977d422a88ed51d58c5594009df
sdamman <sdamman@dpswebapps.com>
2022-05-12 11:35:49 -0500

    Added checker class

--- source/TimerTest/Timer.WPF/Classes/Checker.cs
+++ source/TimerTest/Timer.WPF/Classes/Checker.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Timer.WPF.Classes
 {
 	public class Checker
 	{
+		private int invokeCount;
+		private int maxCount;
+
+		public Checker(int count)
+		{
+			invokeCount = 0;
+			maxCount = count;
+		}
+
+		public async void Check(Object stateInfo)
+		{
+			AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+			Console.WriteLine($"{DateTime.Now:h:mm:ss.ff} Checking status {++invokeCount}");
+
+			if (invokeCount == maxCount)
+			{
+				invokeCount = 0;
+				autoEvent.Set();
+			}
+		}
 
 	}
 }
